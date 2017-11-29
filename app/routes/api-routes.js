ebc8fd6ddf4622c3f6ba4eb30b567a79cd902caf
Stephanie Wertz <stephaniewertz@Stephanies-MacBook-Air.local>
2017-11-28 23:04:12 -0600

    tuesday night updates

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -12,29 +12,47 @@ var connection = require("../config/connection.js");
 module.exports = function(app) {
 
   // Get country school is located in
+  
 
   app.get("/api/location", function(req, res) {
 
+     var USA = 0;
     var dbQuery = "SELECT * FROM school__field_address_location";
 
     connection.query(dbQuery, function(err, result) {
       res.json(result);
       console.log(result)
+
+
+      // Count for How many schools are in the US
+      
+      for (var i = 0; i < result.length; i++) {
+        result.field_address_location_country_code = "US"
+        USA = USA + 1;
+      }
+
+      console.log(USA);
+     
+
     });
 
   });
+
+  //Get Total Users 
 
   app.get("/api/totusers", function(req, res) {
-    var dbQuery = "SELECT created FROM users_field_data";
+    var dbQuery = "SELECT from_unixtime(created) AS userSince FROM users_field_data WHERE created BETWEEN 1483228800 AND 1511564538";
 
     connection.query(dbQuery, function(err, result) {
       res.json(result);
       console.log(result)
     });
   });
+
+  //Get Actual Users
 
   app.get("/api/actusers", function(req, res) {
-    var dbQuery = "SELECT uid, name AS username, mail, from_unixtime(access) AS lastAccess, from_unixtime(login) AS lastLogin FROM users_field_data WHERE created BETWEEN 1483228800 AND 1511564536";
+    var dbQuery = "SELECT from_unixtime(access) AS lastAccess, from_unixtime(login) AS lastLogin FROM users_field_data WHERE created BETWEEN 1483228800 AND 1511564538";
 
     connection.query(dbQuery, function(err, result) {
       res.json(result);
@@ -42,21 +60,43 @@ module.exports = function(app) {
     });
   });
 
+  //Get Submitted Today
 
+  app.get("/api/subtoday", function(req, res) {
+    var dbQuery = "SELECT revision_id FROM node__field_state";
+
+    connection.query(dbQuery, function(err, result) {
+      res.json(result);
+      console.log(result)
+    });
+  });
+
+  //Get Total Submitted
+
+  app.get("/api/totsub", function(req, res) {
+    var dbQuery = "SELECT field_state_value FROM node__field_state";
+
+    connection.query(dbQuery, function(err, result) {
+      res.json(result);
+      console.log(result)
+
+    });
+  });
+  
+  //Get Most Active Schools
+
+  app.get("/api/actschools", function(req, res) {
+    var dbQuery = "SELECT field_school_name_value FROM user__field_school_name";
+
+    connection.query(dbQuery, function(err, result) {
+      res.json(result);
+      console.log(result)
+
+    });
+  });
+
+  //Need Route for User Age
 
   
-  // app.post("/api/new", function(req, res) {
-
-  //   console.log("benchmark data:");
-  //   console.log(req.body);
-
-  //   var dbQuery = "INSERT INTO benchmark (author, body, created_at) VALUES (?,?,?)";
-
-  //   connection.query(dbQuery, [req.body.author, req.body.body, req.body.created_at], function(err, result) {
-  //     console.log("benchmark Successfully Saved!");
-  //     res.end();
-  //   });
-
-  // });
 
 };
