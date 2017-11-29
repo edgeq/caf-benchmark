7bcef6791196d1f5eae1684fdb4c50256755c674
Casey Aitken <caseyaitken@gmail.com>
2017-11-29 12:48:25 -0600

    Updated UI

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,24 +1,37 @@
 import React, { Component } from "react";
 
+import Routes from "../../../app/routes/api-routes"
+
 const SideBar = () => {
+
+	// const Routes.app.get
 	return (
 		<div id="sidebar">
-			<p>Overview</p>
-			<div class="sidebarBox">
-				<p>Active Users</p>
-				<p class="sidebarNumber">99999</p>
-			</div>
-			<div class="sidebarBox">
-				<p>Total Users</p>
-				<p class="sidebarNumber">99999</p>
-			</div>
-			<div class="sidebarBox">
-				<p>Total Submissions</p>
-				<p class="sidebarNumber">999</p>
-			</div>
-			<div class="sidebarBox">
-				<p>Submissions Today</p>
-				<p class="sidebarNumber">999</p>
+			<div class="row">
+				<div className="sidebarBox">
+				<h2>Overview</h2>
+					<p>Active Users</p>
+					<p className="sidebarNumber">99999</p>
+				</div>
+				<div className="sidebarGraph"></div>
+				<div className="separator"></div>
+				<div className="sidebarBox">
+					<p>Total Users</p>
+					<p className="sidebarNumber">99999</p>
+				</div>
+				<div className="sidebarGraph"></div>
+				<div className="separator"></div>
+				<div className="sidebarBox">
+					<p>Total Submissions</p>
+					<p className="sidebarNumber">999</p>
+				</div>
+				<div className="sidebarGraph"></div>
+				<div className="separator"></div>
+				<div className="sidebarBox">
+					<p>Submissions Today</p>
+					<p className="sidebarNumber">999</p>
+				</div>
+				<div className="sidebarGraph"></div>
 			</div>
 		</div>
 	);
