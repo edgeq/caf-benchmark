b72d7493fab9e19721b7eb0872fdfc18786eed2b
Edge Quintanilla <edgarq86@gmail.com>
2017-12-04 14:26:46 -0600

    not super broken but not working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -25,27 +25,28 @@ export default class Login extends Component {
     return (
       <div className="col-sm-6 col-sm-offset-6 loginBoxContent">
         <h1 style={{color: "white"}}> Login </h1>
-        
-        <form onSubmit={this.handleSubmit}>
+        <div className="container-fluid">
+          <form onSubmit={this.handleSubmit}>
 
-          <div className="form-group">
-            <label>Email</label>
-            <input className="form-control" ref={(email) => this.email = email} placeholder="Email"/>
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
-          </div>
-          {
-            this.state.loginMessage &&
-            <div className="alert alert-danger" role="alert">
-              <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-              <span className="sr-only">Error:</span>
-              &nbsp;{this.state.loginMessage} <a href="#" onClick={this.resetPassword} className="alert-link">Forgot Password?</a>
+            <div className="form-group">
+              <label>Email</label>
+              <input className="form-control" ref={(email) => this.email = email} placeholder="Email"/>
             </div>
-          }
-          <button type="submit" className="btn btn-primary">Login</button>
-        </form>
+            <div className="form-group">
+              <label>Password</label>
+              <input type="password" className="form-control" placeholder="Password" ref={(pw) => this.pw = pw} />
+            </div>
+            {
+              this.state.loginMessage &&
+              <div className="alert alert-danger" role="alert">
+                <span className="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+                <span className="sr-only">Error:</span>
+                &nbsp;{this.state.loginMessage} <a href="#" onClick={this.resetPassword} className="alert-link">Forgot Password?</a>
+              </div>
+            }
+            <button type="submit" className="btn btn-primary">Login</button>
+          </form>
+        </div>
       </div>
     )
   }
---
+++ client/src/components/index.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+import { Route, BrowserRouter, Link, Redirect, Switch } from 'react-router-dom'
+import NavBar from './Navbar'
+import Login from './Login'
+import Register from './Register'
+// import Dashboard from './protected/Dashboard'
+import { logout } from '../helpers/auth'
+import { firebaseAuth } from '../config/constants'
+
+function PrivateRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === true
+        ? <Component {...props} />
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
+    />
+  )
+}
+
+function PublicRoute ({component: Component, authed, ...rest}) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => authed === false
+        ? <Component {...props} />
+        : <Redirect to='/' />}
+    />
+  )
+}
+
+export default class App extends Component {
+  state = {
+    authed: false,
+    loading: true,
+  }
+  componentDidMount () {
+    this.removeListener = firebaseAuth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authed: true,
+          loading: false,
+        })
+      } else {
+        this.setState({
+          authed: false,
+          loading: false
+        })
+      }
+    })
+  }
+  componentWillUnmount () {
+    this.removeListener()
+  }
+  render() {
+    <NavBar />
+    return this.state.loading === true ? <h1>Loading</h1> : (
+      <BrowserRouter>
+        <div>
+          <nav className="navbar navbar-default navbar-static-top">
+            <div className="container">
+              <div className="navbar-header">
+                <Link to="/" className="navbar-brand">CAF Benchmark</Link>
+              </div>
+              <ul className="nav navbar-nav pull-right">
+                <li>
+                  <Link to="/" className="navbar-brand">Home</Link>
+                </li>
+                <li>
+                  <Link to="/" className="navbar-brand">Dashboard</Link>
+                </li>
+                <li>
+                  {this.state.authed
+                    ? <button
+                      style={{border: 'none', background: 'transparent'}}
+                      onClick={() => {
+                        logout()
+                      }}
+                      className="navbar-brand">Logout</button>
+                    : <span>
+                      <Link to="/login" className="navbar-brand">Login</Link>
+                      <Link to="/register" className="navbar-brand">Register</Link>
+                    </span>}
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div className="container">
+            <div className="row">
+              <Switch>
+                <Route path='/' exact component={Login} />
+                <PublicRoute authed={this.state.authed} path='/login' component={Login} />
+                <PublicRoute authed={this.state.authed} path='/register' component={Register} />
+                <PrivateRoute authed={this.state.authed} path='/' component={Login} />
+                <Route render={() => <h3>No Match</h3>} />
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
